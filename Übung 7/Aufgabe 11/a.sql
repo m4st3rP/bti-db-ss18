0ce9230b0b0a05d7ff1658c7592495ffc20b36b0
Philipp Schwarz <schwarz_philipp@hotmail.de>
2018-06-26 16:12:51 +0200

    added documentational comments to exercise 7 task 11

--- Übung 7/Aufgabe 11/a.sql
+++ Übung 7/Aufgabe 11/a.sql
@@ -1,3 +1,6 @@
+/*
+ * Wir sortieren die Kunden alphabetisch aufsteigend über ihren Namen damit wir sie schneller über ihren Namen finden können.
+ */
 CREATE INDEX namen_suche
 ON Kunde (Name asc);
 
@@ -7,6 +10,9 @@ WHERE NAME LIKE 'P%';
 
 
 
+/*
+ * Wir sortieren die Produkte aufsteigend über ihre einzigartige Artikelnummer damit wir sie schneller über ihre Artikelnummer finden können.
+ */
 CREATE UNIQUE INDEX artikelnummer_suche
 ON Produkt (Artikelnummer asc);
 
@@ -16,6 +22,9 @@ WHERE Artikelnummer = 2;
 
 
 
+/*
+ * Wir sortieren die Bestellungen absteigend über ihre einzigartige Bestellnummer, da uns neuere Bestellungen eher interessieren, damit wir sie schneller über ihre Bestellnummer finden können.
+ */
 CREATE UNIQUE INDEX desc_bestellnummer_suche
 ON Bestellungen (Bestellnummer desc);
 
